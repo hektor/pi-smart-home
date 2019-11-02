1f95ecda3202431b0340c4dae05aadf4277a2df4
hektormisplon <hektor.misplon@gmail.com>
2019-11-02 11:45:46 +0100

    Togglable component

---
+++ client/src/Togglable.svelte
@@ -0,0 +1,21 @@
+<script>
+  // Firebase
+  import { firestore } from './firebase'
+  export let turnedOn
+  export let id
+  function toggleStatus() {
+    turnedOn = !turnedOn
+    const lightSetting = firestore
+      .collection('devices')
+      .doc('lights')
+      .update({
+        [id]: turnedOn,
+      })
+  }
+</script>
+
+{#if turnedOn}
+  <button on:click={toggleStatus}>⚪</button>
+{:else}
+  <button on:click={toggleStatus}>⚫</button>
+{/if}
