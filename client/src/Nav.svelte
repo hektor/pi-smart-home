f220c7ec3d4a7196ec409ca6fea1b64b5ce47d51
hektormisplon <hektor.misplon@gmail.com>
2019-11-02 16:11:20 +0100

    styling

--- client/src/Nav.svelte
+++ client/src/Nav.svelte
@@ -4,44 +4,45 @@
     ZapIcon,
     ShieldIcon,
     KeyIcon,
-    ActivityIcon
-  } from 'svelte-feather-icons';
+    ActivityIcon,
+  } from 'svelte-feather-icons'
 </script>
 
 <style>
-  nav {
-    display: flex;
-    position: fixed;
-    overflow: hidden;
-    bottom: 0;
+  .nav-item {
+    @apply p-2;
+  }
+  a {
+    transition: all 120ms cubic-bezier(0.5, 0.82, 0.165, 1);
+    @apply text-gray-800;
   }
 
-  a {
-    flex: 1;
-    text-align: center;
-    padding: 1rem;
+  a:hover {
+    @apply text-gray-500;
   }
 </style>
 
-<nav>
-  <a href="/">
-    <SunIcon />
-    Lights
-  </a>
-  <a href="/">
-    <ZapIcon />
-    Sockets
-  </a>
-  <a href="/">
-    <KeyIcon />
-    Doors
-  </a>
-  <a href="/">
-    <ActivityIcon />
-    Sensors
-  </a>
-  <a href="/">
-    <ShieldIcon />
-    Security
-  </a>
-</nav>
+<div
+  class="w-full h-16 flex flex-row fixed bottom-0 md:flex-col md:w-16 md:left-0
+  md:flex-none md:top-0 md:my-32">
+  <div class="nav-item flex-1">
+    <a href="/lights">
+      <SunIcon />
+    </a>
+  </div>
+  <div class="nav-item flex-1">
+    <a href="/plugs">
+      <ZapIcon />
+    </a>
+  </div>
+  <div class="nav-item flex-1">
+    <a href="/doors">
+      <KeyIcon />
+    </a>
+  </div>
+  <div class="nav-item flex-1">
+    <a href="/security">
+      <ShieldIcon />
+    </a>
+  </div>
+</div>
