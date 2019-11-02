d4210991516d95bda926e080d89bfdfb778db43d
hektormisplon <hektor.misplon@gmail.com>
2019-11-02 17:14:37 +0100

    styling

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -48,6 +48,14 @@
   .toggle-group {
     flex: 1;
   }
+
+  button {
+    transition: all 120ms cubic-bezier(0.5, 0.82, 0.165, 1);
+  }
+
+  button:hover {
+    @apply bg-gray-700;
+  }
 </style>
 
 <Header {title} />
@@ -71,10 +79,28 @@
     </div>
     <div>
       {#if user}
-        <button on:click={() => auth.signOut()}>Sign out</button>
+        <button
+          on:click={() => auth.signOut()}
+          class="bg-gray-800 text-white font-bold rounded
+          hover:border-yellow-600 hover:bg-yellow-500 hover:text-white shadow-lg
+          py-2 px-6 inline-flex items-center m-2">
+          Sign out
+        </button>
       {:else}
-        <button on:click={() => auth.signInWithPopup(googleProvider)}>
-          Sign in with Google
+        <button
+          on:click={() => auth.signInWithPopup(googleProvider)}
+          class="bg-gray-800 text-white font-bold rounded shadow-lg py-2 px-6
+          inline-flex items-center m-2">
+          <span class="mr-2">Sign in with Google</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24">
+            <path
+              fill="currentcolor"
+              d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z" />
+          </svg>
         </button>
       {/if}
     </div>
--- client/src/Togglable.svelte
+++ client/src/Togglable.svelte
@@ -26,8 +26,19 @@
   }
 </script>
 
-{#if status}
-  <button on:click={toggleStatus}>⚪</button>
-{:else}
-  <button on:click={toggleStatus}>⚫</button>
-{/if}
+<style>
+  button {
+    transition: all 120ms cubic-bezier(0.5, 0.82, 0.165, 1);
+    @apply bg-gray-800 m-2 w-8 h-8 rounded shadow-lg;
+  }
+
+  button:hover {
+    @apply bg-gray-700;
+  }
+
+  .active {
+    background: #fff;
+  }
+</style>
+
+<button on:click={toggleStatus} class={status ? 'active' : ''} />
