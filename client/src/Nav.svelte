afa24318a5a66241f19096ada14688805c99ac9b
hektormisplon <hektor.misplon@gmail.com>
2019-11-02 11:45:57 +0100

    tab bar

---
+++ client/src/Nav.svelte
@@ -0,0 +1,47 @@
+<script>
+  import {
+    SunIcon,
+    ZapIcon,
+    ShieldIcon,
+    KeyIcon,
+    ActivityIcon
+  } from 'svelte-feather-icons';
+</script>
+
+<style>
+  nav {
+    display: flex;
+    position: fixed;
+    overflow: hidden;
+    bottom: 0;
+  }
+
+  a {
+    flex: 1;
+    text-align: center;
+    padding: 1rem;
+  }
+</style>
+
+<nav>
+  <a href="/">
+    <SunIcon />
+    Lights
+  </a>
+  <a href="/">
+    <ZapIcon />
+    Sockets
+  </a>
+  <a href="/">
+    <KeyIcon />
+    Doors
+  </a>
+  <a href="/">
+    <ActivityIcon />
+    Sensors
+  </a>
+  <a href="/">
+    <ShieldIcon />
+    Security
+  </a>
+</nav>
