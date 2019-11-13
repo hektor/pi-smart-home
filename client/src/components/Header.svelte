35d64361e66a511e83a177361b7410dd4af469f5
hektormisplon <hektor.misplon@gmail.com>
2019-11-13 02:56:56 +0100

    google sign in header

--- client/src/components/Header.svelte
+++ client/src/components/Header.svelte
@@ -1,11 +1,26 @@
 <script>
   export let title
   import FirebaseLogo from '../assets/FirebaseLogo.svelte'
+
+  // Firebase
+  import { auth, googleProvider, firestore } from '../firebase'
+  import { authState } from 'rxfire/auth'
+  let user
+  const unsubscribe = authState(auth).subscribe(u => (user = u))
 </script>
 
 <style>
   header {
-    @apply shadow-xl flex-1;
+    @apply shadow-xl flex justify-between;
+  }
+
+  button {
+    @apply bg-gray-800 text-white font-bold rounded  shadow-lg py-2 px-6 inline-flex
+        items-center;
+  }
+
+  button:hover {
+    @apply border-yellow-600 bg-yellow-500 text-white;
   }
 </style>
 
@@ -13,4 +28,13 @@
   <a href="/">
     <FirebaseLogo />
   </a>
+  <div>
+    {#if user}
+      <button on:click={() => auth.signOut()}>Sign out</button>
+    {:else}
+      <button on:click={() => auth.signInWithPopup(googleProvider)}>
+        <span class="mr-2">Sign in with Google</span>
+      </button>
+    {/if}
+  </div>
 </header>
